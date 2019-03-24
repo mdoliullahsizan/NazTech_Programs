d2c1f250ede90ac07cfc2599774a7f16ce938fca
Md. Oliullah Sizan <43073654+mdoliullahsizan@users.noreply.github.com>
2019-03-24 10:56:42 +0600

    Add files via upload

---
+++ Array[03Mar19]/src/Array.java
@@ -0,0 +1,18 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		String array[]= {"Sizan ", "23 ", "318 ", "Trainee ", "0184 "};
+		//System.out.println(array[1]);
+		
+		for(int i= 0; i<=4; i++) {
+			System.out.println(array[i]);
+		}
+		
+//		for(int i= 0; i<=array[i].length; i++) {
+//			System.out.println(array[i]);
+//		}
+
+	}
+
+}
