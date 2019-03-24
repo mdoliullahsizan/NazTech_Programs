c4c6de14a8a0d478b60e08267f955d1600a4e5ff
Md. Oliullah Sizan <43073654+mdoliullahsizan@users.noreply.github.com>
2019-03-24 11:15:07 +0600

    Add files via upload

---
+++ Java_Collections[13Feb19]/src/Main/Main.java
@@ -0,0 +1,12 @@
+package Main;
+import Encapsulation.encap;
+
+public class Main {
+
+	public static void main(String[] args) {
+		encap in= new encap();
+		in.setName("Md. Oliullah Sizan");
+System.out.println(in.getName());
+	}
+
+}
